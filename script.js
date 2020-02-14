8ec702ffa62d505a5e748096219e8d21d47c3756
MrHaika <morganra92@hotmail.com>
2020-02-14 13:44:03 +0100

    added styling

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $.ajax({
-    type: "GET",
-    url: "https://my-json-server.typicode.com/MrAntonsen/jqDb/posts",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    success: function(res) {
-      const $posts = $(".output-list");
+	$.ajax({
+		type: 'GET',
+		url: 'https://my-json-server.typicode.com/MrAntonsen/jqDb/posts',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		success: function(res) {
+			const $posts = $('.output-list');
 
-      $(res).each((i, post) => {
-        let postcard = `
-              <div class="card" id="${i}_post">
+			$(res).each((i, post) => {
+				let postcard = `
+              <div class="card gradient-border mb-5 mt-1" id="${i}_post">
                 <div class="card-body">
                   <h3 class="card-title text-center font-weight-bold">
                     ${post.title}
@@ -18,13 +18,13 @@ $(document).ready(function() {
                 </div>
               </div>
               `;
-        $posts.on("click", () => {
-          console.log(post.userId);
-          sessionStorage.setItem("userId", post.id);
-          window.location = "posts.html";
-        });
-        $posts.append(postcard);
-      });
-    }
-  });
+				$posts.on('click', () => {
+					console.log(post.userId);
+					sessionStorage.setItem('userId', post.id);
+					window.location = 'posts.html';
+				});
+				$posts.append(postcard);
+			});
+		}
+	});
 });
--- scriptPost.js
+++ scriptPost.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  const Userid = sessionStorage.getItem("userId");
-  let liItem;
-  let details = {};
-  let postItem;
-  $.ajax({
-    type: "get",
-    url: `https://my-json-server.typicode.com/MrAntonsen/jqDb/users/${Userid}`,
-    headers: {
-      "Content-Type": "application/json"
-    },
+	const Userid = sessionStorage.getItem('userId');
+	let liItem;
+	let details = {};
+	let postItem;
+	$.ajax({
+		type: 'get',
+		url: `https://my-json-server.typicode.com/MrAntonsen/jqDb/users/${Userid}`,
+		headers: {
+			'Content-Type': 'application/json'
+		},
 
-    success: function(response) {
-      liItem = `<div class="row">
+		success: function(response) {
+			liItem = `<div class="row">
           <div class="col-md-8 offset-2 text-center">
               <h1 class="mb-5">${response.userName}</h1>
           </div>
@@ -22,25 +22,25 @@ $(document).ready(function() {
           <div class="col-sm-3"><span>Gender:</span>  ${response.userGender}</div>
           <div class="col-sm-3"><span>Age:</span>  ${response.userAge}</div>
           </div>`;
-    }
-  });
+		}
+	});
 
-  $.ajax({
-    type: "get",
-    url: `https://my-json-server.typicode.com/MrAntonsen/jqDb/posts/${Userid}`,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    success: function(response) {
-      details.userId = response.userId;
-      details.id = response.id;
-      details.title = response.title;
-      details.body = response.body;
-      console.log(details);
-      postItem = PostItem(details);
-      let postoutput = `<div class="row">
+	$.ajax({
+		type: 'get',
+		url: `https://my-json-server.typicode.com/MrAntonsen/jqDb/posts/${Userid}`,
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		success: function(response) {
+			details.userId = response.userId;
+			details.id = response.id;
+			details.title = response.title;
+			details.body = response.body;
+			console.log(details);
+			postItem = PostItem(details);
+			let postoutput = `<div class="row">
       <div class="col-sm-6 offset-3">
-        <div class="card">
+        <div class="card gradient-border">
         <div class="card-body text-center">
         <div class="card-title"><h3>${postItem.getTitle()}<h3></div>
             ${postItem.getBody()}
@@ -48,23 +48,23 @@ $(document).ready(function() {
         </div>
       </div>
     </div>`;
-      $(".post-container").append(postoutput);
-    }
-  });
+			$('.post-container').append(postoutput);
+		}
+	});
 
-  getUserInfo = () => {
-    $("#exampleModal1").modal("show");
-    setTimeout(() => {
-      $("#exampleModal1").modal("hide");
-    }, 4000);
-    $("#exampleModal").modal("hide");
-    setTimeout(() => {
-      $("#exampleModal").html(
-        `
+	getUserInfo = () => {
+		$('#exampleModal1').modal('show');
+		setTimeout(() => {
+			$('#exampleModal1').modal('hide');
+		}, 4000);
+		$('#exampleModal').modal('hide');
+		setTimeout(() => {
+			$('#exampleModal').html(
+				`
           <div class="modal-dialog user-modal modal-lg" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title text-center" id="exampleModalLabel">Post title</h5>
+                <h5 class="modal-title text-center" id="exampleModalLabel">User Information</h5>
                 <button
                   type="button"
                   class="close"
@@ -80,28 +80,28 @@ $(document).ready(function() {
             </div>
           </div>
         `
-      );
-      $(".user-modal-body").append(liItem);
-      $("#exampleModal").modal("toggle");
-    }, 4000);
-  };
+			);
+			$('.user-modal-body').append(liItem);
+			$('#exampleModal').modal('toggle');
+		}, 4000);
+	};
 });
 
-let PostItem = item => {
-  this.userId = item.userId;
-  this.id = item.id;
-  this.title = item.title;
-  this.body = item.body;
+let PostItem = (item) => {
+	this.userId = item.userId;
+	this.id = item.id;
+	this.title = item.title;
+	this.body = item.body;
 
-  return {
-    getTitle: () => {
-      return this.title;
-    },
-    getBody: () => {
-      return this.body;
-    },
-    getId: () => {
-      return this.id;
-    }
-  };
+	return {
+		getTitle: () => {
+			return this.title;
+		},
+		getBody: () => {
+			return this.body;
+		},
+		getId: () => {
+			return this.id;
+		}
+	};
 };
